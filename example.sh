6b0b7ca67d7a24f9b9681ae8946b9b179d287fd7
Jonathan Champ <jrchamp@ncsu.edu>
2018-08-01 13:26:20 -0400

    delta: Initial ruleset aligned to tech specs

--- example.sh
+++ example.sh
@@ -1,2 +1,11 @@
 # Go to the directory you want to check and run this:
 /misc/temp/phpcs/PHP_CodeSniffer/bin/phpcs -p --colors --standard=/misc/temp/phpcs/delta.xml --basepath=. .
+
+# To apply the fixes which can be automatically applied:
+# /misc/temp/phpcs/PHP_CodeSniffer/bin/phpcbf -p --colors --standard=/misc/temp/phpcs/delta.xml --basepath=. .
+
+# To only show errors and not warnings:
+# /misc/temp/phpcs/PHP_CodeSniffer/bin/phpcs -n -p --colors --standard=/misc/temp/phpcs/delta.xml --basepath=. .
+
+# To show which sniffs are triggering the messages:
+# /misc/temp/phpcs/PHP_CodeSniffer/bin/phpcs -s -p --colors --standard=/misc/temp/phpcs/delta.xml --basepath=. .
